e96eecae10e1026fe48dd14a39286a4e6ff265e1
funnyboy-roks <funnyboyroks@gmail.com>
2021-10-22 19:34:31 -0500

    Updates

--- JavaClient/src/com/funnyboyroks/Main2.java
+++ JavaClient/src/com/funnyboyroks/Main2.java
@@ -1,16 +1,14 @@
-package com.funnyboyroks;
-
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class Main {
+public class Main2 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println(Arrays.stream(in.nextLine().split(" ")).map(Main::reverseString).collect(Collectors.joining(" ")));
+        System.out.println(Arrays.stream(in.nextLine().split(" ")).map(Main2::reverseString).collect(Collectors.joining(" ")));
     }
 
     public static String reverseString(String str) {
